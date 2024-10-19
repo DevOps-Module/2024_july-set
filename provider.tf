7348adbd3f7698f7c8383b3a73f9466518af0017
mobanntechnologies <mobanntechnologies@gmail.com>
2024-10-19 20:16:05 +0000

    modified_provider.tf

--- provider.tf
+++ provider.tf
@@ -1,2 +1,21 @@
-provider
-{aws}
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "eu-west-2"
+#  access_key = "my-access-key"
+#  secret_key = "my-secret-key"
+}
+
+
